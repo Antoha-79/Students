882c2d916b452fcf128f1060d388c592d4ec5791
Anton Dovnar <adam_s@mail.ru>
2022-03-18 21:58:33 +0300

    мелочь

--- Dovnar-Students-CoreData/Modules/Cells/StudOfCoursTableVCell.swift
+++ Dovnar-Students-CoreData/Modules/Cells/StudOfCoursTableVCell.swift
@@ -20,7 +20,6 @@ class StudOfCoursTableVCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-      
     }
 
 */
